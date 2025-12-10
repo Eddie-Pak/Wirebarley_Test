319c0633697ff2ee468d2a7279d6e94624c4f897
Eddie-Pak <foalstn@naver.com>
2025-12-11 02:32:41 +0900

    refactor: data class 수정

--- domain/src/main/java/com/wirebarley/domain/model/ExchangeData.kt
+++ domain/src/main/java/com/wirebarley/domain/model/ExchangeData.kt
@@ -6,6 +6,5 @@ data class ExchangeData(
     val currentRate: Double = 0.0,
     val sendAmount: String = "",
     val receiveAmount: String = "",
-    val formattedRate: String = "0.00",
-    val queryTime: String = ""
+    val timestamp: Long = 0L
 )
