db13c559189c9eb30f475e3909862e6b3a3c0005
Eddie-Pak <foalstn@naver.com>
2025-12-11 18:20:15 +0900

    refactor: QueryParameter companion object 생성

--- data/src/main/java/com/wirebarley/data/api/ExchangeAuthInterceptor.kt
+++ data/src/main/java/com/wirebarley/data/api/ExchangeAuthInterceptor.kt
@@ -8,15 +8,26 @@ import javax.inject.Named
 class ExchangeAuthInterceptor @Inject constructor(
     @Named("apiKey") private val apiKey: String
 ) : Interceptor {
+    companion object {
+        private const val QUERY_ACCESS_KEY = "access_key"
+        private const val QUERY_CURRENCIES = "currencies"
+        private const val QUERY_SOURCE = "source"
+        private const val QUERY_FORMAT = "format"
+
+        private const val DEFAULT_CURRENCIES = "KRW,JPY,PHP"
+        private const val DEFAULT_SOURCE = "USD"
+        private const val DEFAULT_FORMAT = "1"
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val originalUrl = originalRequest.url
 
         val newUrl = originalUrl.newBuilder()
-            .addQueryParameter("access_key", apiKey)
-            .addQueryParameter("currencies", "KRW,JPY,PHP")
-            .addQueryParameter("source", "USD")
-            .addQueryParameter("format", "1")
+            .addQueryParameter(QUERY_ACCESS_KEY, apiKey)
+            .addQueryParameter(QUERY_CURRENCIES, DEFAULT_CURRENCIES)
+            .addQueryParameter(QUERY_SOURCE, DEFAULT_SOURCE)
+            .addQueryParameter(QUERY_FORMAT, DEFAULT_FORMAT)
             .build()
 
         val newRequest = originalRequest.newBuilder()
