6535c6653cf405dbf54342ecae374bb0f5f9b3d5
Eddie-Pak <foalstn@naver.com>
2025-12-12 19:26:48 +0900

    refactor: ExchangeApiResponse Success, Error 분리

--- data/src/main/java/com/wirebarley/data/mapper/Exchangeratemapper.kt
+++ data/src/main/java/com/wirebarley/data/mapper/Exchangeratemapper.kt
@@ -1,22 +1,27 @@
 package com.wirebarley.data.mapper
 
-import com.wirebarley.data.dto.ExchangeApiResponse
+import com.wirebarley.data.dto.ExchangeErrorResponse
+import com.wirebarley.data.dto.ExchangeSuccessResponse
 import com.wirebarley.domain.common.FormatUtil
 import com.wirebarley.domain.model.Currency
 import com.wirebarley.domain.model.ExchangeRate
 
-fun ExchangeApiResponse.toDomain(): ExchangeRate {
-    val rates = quotes?.mapNotNull { (key, value) ->
+fun ExchangeSuccessResponse.toDomain(): ExchangeRate {
+    val rates = quotes.mapNotNull { (key, value) ->
         // "USDKRW" → "KRW" 추출
         val currencyCode = key.removePrefix("USD")
 
         val currency = Currency.entries.find { it.code == currencyCode }
 
         currency?.let { it to FormatUtil.formatCurrency(value) }
-    }?.toMap() ?: emptyMap()
+    }.toMap()
 
     return ExchangeRate(
         rates = rates,
-        timestamp = timestamp ?: 0L
+        timestamp = timestamp
     )
+}
+
+fun ExchangeErrorResponse.toErrorMessage(): String {
+    return this.error.info
 }
