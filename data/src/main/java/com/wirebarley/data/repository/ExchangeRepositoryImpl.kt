2a6aeddacfe28266565ed72446f532566f0bd801
Eddie-Pak <foalstn@naver.com>
2025-12-13 20:49:09 +0900

    refactor: ExchangeRepositoryImpl success, error 분기 구현

--- data/src/main/java/com/wirebarley/data/repository/ExchangeRepositoryImpl.kt
+++ data/src/main/java/com/wirebarley/data/repository/ExchangeRepositoryImpl.kt
@@ -1,28 +1,34 @@
 package com.wirebarley.data.repository
 
 import com.wirebarley.data.api.ExchangeApi
+import com.wirebarley.data.dto.ExchangeErrorResponse
+import com.wirebarley.data.dto.ExchangeSuccessResponse
 import com.wirebarley.data.mapper.toDomain
+import com.wirebarley.data.mapper.toErrorMessage
 import com.wirebarley.domain.common.ApiResult
 import com.wirebarley.domain.repository.ExchangeRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class ExchangeRepositoryImpl @Inject constructor(
     private val exchangeApi: ExchangeApi,
 ) : ExchangeRepository {
     override fun getExchangeRates() = flow {
-        emit(ApiResult.Loading)
+        when (val response = exchangeApi.getExchangeRates()) {
+            is ExchangeSuccessResponse -> {
+                emit(ApiResult.Success(response.toDomain()))
+            }
 
-        val response = exchangeApi.getExchangeRates()
-
-        if (response.success) {
-            emit(ApiResult.Success(response.toDomain()))
-        } else {
-            emit(ApiResult.Error(response.error?.info ?: "잠시 후 다시 시도해주세요."))
+            is ExchangeErrorResponse -> {
+                emit(ApiResult.Error(response.toErrorMessage()))
+            }
         }
+    }.onStart {
+        emit(ApiResult.Loading)
     }.catch { e ->
         emit(ApiResult.Error(e.message ?: "잠시 후 다시 시도해주세요."))
     }.flowOn(Dispatchers.IO)
